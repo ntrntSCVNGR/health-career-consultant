aebf9045d28ab1ef97c604f8b22e1f995c7ea43a
ntrntSCVNGR <223327466+ntrntSCVNGR@users.noreply.github.com>
2025-09-20 01:45:09 +0300

    Build ready for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import vue from '@vitejs/plugin-vue'
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // default, but explicit for clarity
-  },
+  base: '/health-career-consultant/', // <-- This is important for GitHub Pages
+  plugins: [vue()],
 })
